07fe972299289da04648fd187bcc5b61f2ff5e24
Dat Le <tuandatle2005@gmail.com>
2026-09-14 18:08:30 -0700

    Add: validate-parentheses - submission-12

---
+++ Data Structures & Algorithms/validate-parentheses/submission-12.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        para = ["(",")","{","}","[","]"]
+        stor = []
+        if len(s)%2==1:
+            return False
+        for i in range(len(s)):
+            if s[i]=='[' or s[i]=='(' or s[i]=='{':
+                stor.append(s[i])
+            elif stor:
+                if s[i]==']' and stor[-1] == '[':
+                    stor.pop()
+                elif s[i]==')' and stor[-1] == '(':
+                    stor.pop()
+                elif s[i]=='}' and stor[-1] == '{':
+                    stor.pop()
+                else:
+                    return False
+            else:
+                return False
+        return len(stor)==0
