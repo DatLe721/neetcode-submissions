bdb50843de8503e4315bf347ea205252ea82ffab
Dat Le <tuandatle2005@gmail.com>
2026-09-22 14:25:24 -0700

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,24 @@
+class MinStack:
+
+    def __init__(self):
+        self.min = []
+        self.stack =[]
+
+    def push(self, val: int) -> None:
+        if not self.min:
+            self.min.append(val)
+        elif self.min[-1]>val:
+            self.min.append(val)
+        else:
+            self.min.append(self.min[-1])
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min[-1]
